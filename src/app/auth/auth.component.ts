e7bae6bed1e521d184fb52063c38f75d01efbea7
khalid latrach <esokhalid@gmail.com>
2022-01-07 12:28:16 +0100

    Bug

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AuthComponent  implements OnInit{
+ 
+  constructor(private authService: AuthService, private router: Router ) { }
+  ngOnInit(){
+    this.authStatus = this.authService.isAuth;
   }
+
+
+  authStatus: boolean | undefined;
+
+onSignIn(){
+  this.authService.signIn().then(
+    
+  )
+}
+
+
+
+
+
+
+
+//  onSignIn() {
+//    this.authService.signIn().then(
+//      () => {
+//        console.log('Sign in successful!');
+//        this.authStatus=this.authService.isAuth;
+//        this.router.navigate(['machines']);
+//      }
+//    );
+//  }
+
+//  onSignOut() {
+//    this.authService.signOut();
+//    this.authStatus = this.authService.isAuth;
+//  }
 
 }
